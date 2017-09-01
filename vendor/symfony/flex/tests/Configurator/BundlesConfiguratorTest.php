5c2785aa6afae94e31096ea8fefb2d472ba1de52
Ryan Weaver <ryan@thatsquality.com>
2017-09-01 08:24:24 +0200

    adding vendors

---
+++ vendor/symfony/flex/tests/Configurator/BundlesConfiguratorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Flex\Tests\Configurator;
+
+require_once __DIR__.'/TmpDirMock.php';
+
+use Symfony\Flex\Configurator\BundlesConfigurator;
+use PHPUnit\Framework\TestCase;
+
+class BundlesConfiguratorTest extends TestCase
+{
+    public function testConfigure()
+    {
+        $configurator = new BundlesConfigurator(
+            $this->getMockBuilder('Composer\Composer')->getMock(),
+            $this->getMockBuilder('Composer\IO\IOInterface')->getMock(),
+            $this->getMockBuilder('Symfony\Flex\Options')->getMock()
+        );
+
+        $recipe = $this->getMockBuilder('Symfony\Flex\Recipe')->disableOriginalConstructor()->getMock();
+
+        $config = sys_get_temp_dir().'/config/bundles.php';
+        @unlink($config);
+        $configurator->configure($recipe, [
+            'FooBundle' => ['dev', 'test'],
+            'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all'],
+        ]);
+        $this->assertEquals(<<<EOF
+<?php
+
+return [
+    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'FooBundle' => ['dev' => true, 'test' => true],
+];
+
+EOF
+        , file_get_contents($config));
+    }
+}
